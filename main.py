0a8d637e4885c74a3759652fdb2eca8575821426
tseringgg <jorjei.ngoche@hotmail.com>
2025-11-14 15:18:22 -0800

    changed newppe command to slash_command type

--- main.py
+++ main.py
@@ -443,16 +443,14 @@ async def on_ready():
             )
         """)
         await db.commit()
-    await bot.tree.sync()
 
-from discord import app_commands
 
-@app_commands.command(name="newppe", description="Create a new PPE (max 10) and make it your active one.")
+@bot.slash_command(name="newppe", description="Create a new PPE (max 10) and make it your active one.")
 # @commands.has_role("PPE Admin")
 @require_ppe_roles(player_required=True)
-async def newppe(interaction: discord.Interaction):
-# async def newppe(ctx: commands.Context):
-    ctx = interaction
+# async def newppe(interaction: discord.Interaction):
+async def newppe(ctx: commands.Context):
+    # ctx = interaction
     guild_id = ctx.guild.id
     records = await load_player_records(guild_id)
     key = ctx.author.display_name.lower()
